57ca3fede49cf07e467d4ff22d978bcf9cae2f10
janbrin <jan.brinkkemper@vng.nl>
2024-09-06 12:38:21 +0200

    a

--- js/config.js
+++ js/config.js
@@ -18,7 +18,7 @@ let respecConfig = {
   //previousPublishDate: "2022-06-24",
   //previousPublishVersion: "0.0.4",
   
-  title: "Ontologie Inkomen",
+  title: "Normafwijking",
 //  subtitle: "Tevens een voorbeeld van een gegenereerd Respec document",
 //  content: {"mermaid": "", "ch01": "informative", "ch02": ""},
 //  authors:
