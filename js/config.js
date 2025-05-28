4f02227a53a9c30d8179b12cd48b85a0db71c4c5
janbrin <jan.brinkkemper@vng.nl>
2025-05-28 14:09:38 +0200

    a

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ let respecConfig = {
   specType: "im",
   
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane specificatie-statussen
-  specStatus: "VV",
+  specStatus: "vv",
   publishDate: "2025-06-01",
   publishVersion: "1.2.0",
 
@@ -20,7 +20,7 @@ let respecConfig = {
   
   title: "Ontologie inkomen",
   subtitle: "",
-  content: {},
+ // content: {},
   authors:
     [
       {
